fda64df11ac0001dd2b46572de036a8334b57bd3
atmoner <contact.atmoner@gmail.com>
2022-05-18 21:50:23 +0200

    Add autoUpdate Commit

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const express = require('express')
+const AutoGitUpdate = require('auto-git-update')
 const app = express()
 const port = 3000
 
+const config = {
+    repository: 'https://github.com/atmoner/test-auto-update',
+    fromReleases: false,
+    tempLocation: './tmp/'
+}
+
 app.get('/', (req, res) => {
-  res.send('Hello atmon3r!')
+  res.send('Hello atmon3r! <a href="./update">update</a>')
 })
+
+app.get('/update', (req, res) => {
+  const updater = new AutoGitUpdate(config);
+  updater.autoUpdate();
+  res.send('update')
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 }) 
+
+
+
+
+
+
