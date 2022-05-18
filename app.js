baaef46283ddecc0abc6f70559cb7f06f03ca21a
atmoner <contact.atmoner@gmail.com>
2022-05-19 00:00:10 +0200

    Change version4

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const express = require('express')
 const AutoGitUpdate = require('auto-git-update')
-const updater = new AutoGitUpdate(config);
+//const updater = new AutoGitUpdate(config);
 const app = express()
 const port = 3000
+const homedir = require('os').homedir();
+console.log(homedir + '/Bureau/test-auto-update3/')
 
 const config = {
     repository: 'https://github.com/atmoner/test-auto-update',
     fromReleases: false,
-    tempLocation: '~/Bureau/test-auto-update3/'
+    tempLocation: homedir + '/Bureau/test-auto-update3/'
 }
 
 app.get('/', (req, res) => {
   var returnData = updater.compareVersions()
-  res.send('Hello atmon3r updated-3! <a href="./update">update</a><br />'+returnData)
+  res.send('Hello atmon3r updated-4! <a href="./update">update</a><br />'+returnData)
 
 })
 
-app.get('/update', (req, res) => {
-  
+app.get('/update', (req, res) => {  
   updater.autoUpdate();
   res.send('update')
 })
